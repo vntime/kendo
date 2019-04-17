cd49fa183f5f1e847609f116370a2300739d1eca
vntime <vntime@gmail.com>
2019-04-18 00:41:40 +0700

    update(manual)

--- WebContent/page/js/grid.js
+++ WebContent/page/js/grid.js
@@ -1,232 +1,129 @@
-var jsonStr = {
-    "country" : [ {
-        "codeLong" : "AUT",
-        "codeShort" : "AT",
-        "name" : "Austria"
+var dataVar = {
+    "data" : [ {
+        "mssv" : 1,
+        "name" : "RP1",
+        "name_en" : "R1",
+        "name_vn" : "1",
+        "age" : 1,
+        "address" : "d1"
     }, {
-        "codeLong" : "BEL",
-        "codeShort" : "BE",
-        "name" : "Belgium"
+        "mssv" : 2,
+        "name" : "RP1",
+        "name_en" : "R2",
+        "name_vn" : "2",
+        "age" : 2,
+        "address" : "d2"
     }, {
-        "codeLong" : "BGR",
-        "codeShort" : "BG",
-        "name" : "Bulgaria"
+        "mssv" : 3,
+        "name" : "RP1",
+        "name_en" : "R3",
+        "name_vn" : "3",
+        "age" : 3,
+        "address" : "d3"
+    }, {
+        "mssv" : 4,
+        "name" : "RP1",
+        "name_en" : "R4",
+        "name_vn" : "4",
+        "age" : 4,
+        "address" : "d4"
     } ]
 };
-var headers = [ {
-    "id" : 1,
-    "column_id" : 0,
-    "title" : "MSSV",
-    "field" : "mssv",
-    "parentColumnId" : 0,
-    "columns" : []
+
+var dataVar2 = [ {
+    "mssv" : 1,
+    "name" : "RP1",
+    "name_en" : "R1",
+    "name_vn" : "1",
+    "age" : 1,
+    "address" : "d1"
 }, {
-    "id" : 2,
-    "column_id" : 0,
-    "title" : "Tên",
-    "field" : "name",
-    "parentColumnId" : 0,
-    "columns" : [ {
-        "id" : 3,
-        "column_id" : 0,
-        "title" : "Tên Tiếng Anh",
-        "field" : "name_en",
-        "parentColumnId" : 0,
-        "columns" : []
-    }, {
-        "id" : 4,
-        "column_id" : 0,
-        "title" : "Tên Tiếng Việt",
-        "field" : "name_vn",
-        "parentColumnId" : 0,
-        "columns" : []
-    } ]
+    "mssv" : 2,
+    "name" : "RP1",
+    "name_en" : "R2",
+    "name_vn" : "2",
+    "age" : 2,
+    "address" : "d2"
 }, {
-    "id" : 5,
-    "column_id" : 0,
-    "title" : "Tuổi",
-    "field" : "age",
-    "parentColumnId" : 0,
-    "columns" : []
+    "mssv" : 3,
+    "name" : "RP1",
+    "name_en" : "R3",
+    "name_vn" : "3",
+    "age" : 3,
+    "address" : "d3"
 }, {
-    "id" : 6,
-    "column_id" : 0,
-    "title" : "Địa chỉ",
-    "field" : "address",
-    "parentColumnId" : 0,
-    "columns" : []
+    "mssv" : 4,
+    "name" : "RP1",
+    "name_en" : "R4",
+    "name_vn" : "4",
+    "age" : 4,
+    "address" : "d4"
 } ];
 
-$(document).ready(function() {
+var columnVar = [ {
+    field : "mssv",
+    title : "mssv",
+    width : "130px"
+}, {
+    field : "name",
+    title : "name",
+    width : "130px"
+}, {
+    field : "name_en",
+    title : "name_en",
+    width : "130px"
+}, {
+    field : "name_vn",
+    title : "name_vn",
+    width : "130px"
+}, {
+    field : "age",
+    title : "age",
+    width : "130px"
+}, {
+    field : "address",
+    title : "address",
+    width : "130px"
+} ];
 
+var dataVarFromServlet;
+
+$(document).ready(function() {
     // Using the core $.ajax() method
     $.ajax({
         // The URL for the request
         url : ReportServletURL,
         // The data to send (will be converted to a query string)
         data : {
-            method : "getHeaders"
+            method : "getRows"
         },
-        async : false,
         // Whether this is a POST or GET request
         type : "POST",
-        // The type of data we expect backq
+        // The type of data we expect back
         dataType : "json",
-        complete : function(data) {
-            // headers = data.responseJSON.data;
+        async : false,
+        complete : function(result) {
+            dataVarFromServlet = result.responseJSON;
         }
     })
 
-    var ds = new kendo.data.DataSource({
-        transport : {
-            read : {
-                // using jsfiddle echo service to simulate JSON endpoint
-                url : ReportServletURL,
-                dataType : "json",
-                type : "POST",
-                data : {
-                    method : "getRows"
-                },
+    $("#grid").kendoGrid({
+        dataSource : {
+            type : "json",
+            // data : dataVar,
+            data : dataVarFromServlet,
+            schema : {
+                data : "data"
             }
         },
-        schema : {
-            data : "data",
-            model : {
-                fields : {
-                    codeLong : {
-                        type : "string"
-                    },
-                    codeShort : {
-                        type : "string"
-                    },
-                    name : {
-                        type : "string"
-                    }
-                }
-            }
-        }
+        height : 550,
+        scrollable : true,
+        sortable : true,
+        filterable : true,
+        pageable : {
+            input : true,
+            numeric : false
+        },
+        columns : columnVar
     });
-
-    $("#grid").kendoGrid({
-        dataSource : ds,
-        height : 180,
-        // scrollable : {
-        // virtual : true
-        // },
-        columns : headers
-    });
-
-    // var ds1 = new kendo.data.DataSource({
-    // transport : {
-    // read : {
-    // // using jsfiddle echo service to simulate JSON endpoint
-    // url : ReportServletURL,
-    // dataType : "json",
-    // type : "POST",
-    // data : {
-    // // /echo/json/ echoes the JSON which you pass as an
-    // // argument
-    // json : JSON.stringify({
-    //    
-    // })
-    // }
-    // }
-    // },
-    // schema : {
-    // data : "country",
-    // model : {
-    // fields : {
-    // codeLong : {
-    // type : "string"
-    // },
-    // codeShort : {
-    // type : "string"
-    // },
-    // name : {
-    // type : "string"
-    // }
-    // }
-    // }
-    // }
-    // });
-    //
-    // var ds2 = new kendo.data.DataSource({
-    // transport : {
-    // read : {
-    // // using jsfiddle echo service to simulate JSON endpoint
-    // url : ReportServletURL,
-    // dataType : "json",
-    // type : "POST",
-    // data : {
-    // // /echo/json/ echoes the JSON which you pass as an
-    // // argument
-    // json : JSON.stringify([ {
-    // "codeLong" : "AUT",
-    // "codeShort" : "AT",
-    // "name" : "Austria"
-    // }, {
-    // "codeLong" : "BEL",
-    // "codeShort" : "BE",
-    // "name" : "Belgium"
-    // }, {
-    // "codeLong" : "BGR",
-    // "codeShort" : "BG",
-    // "name" : "Bulgaria"
-    // } ])
-    // }
-    // }
-    // },
-    // schema : {
-    // model : {
-    // fields : {
-    // codeLong : {
-    // type : "string"
-    // },
-    // codeShort : {
-    // type : "string"
-    // },
-    // name : {
-    // type : "string"
-    // }
-    // }
-    // }
-    // }
-    // });
-    //
-    // $("#grid1").kendoGrid({
-    // dataSource : ds1,
-    // height : 180,
-    // scrollable : {
-    // virtual : true
-    // },
-    // columns : [ {
-    // field : "codeLong",
-    // title : "codeLong"
-    // }, {
-    // field : "codeShort",
-    // title : "codeShort"
-    // }, {
-    // field : "name",
-    // title : "name"
-    // } ]
-    // });
-    //
-    // $("#grid2").kendoGrid({
-    // dataSource : ds2,
-    // height : 180,
-    // scrollable : {
-    // virtual : true
-    // },
-    // columns : [ {
-    // field : "codeLong",
-    // title : "codeLong"
-    // }, {
-    // field : "codeShort",
-    // title : "codeShort"
-    // }, {
-    // field : "name",
-    // title : "name"
-    // } ]
-    // });
-
 });
